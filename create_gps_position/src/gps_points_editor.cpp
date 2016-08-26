653262e6b813911ead4090a331532f42a34feab5
hiroki <wahaha826@gmail.com>
2016-08-27 00:50:10 +0900

    pushback使えた

--- create_gps_position/src/gps_points_editor.cpp
+++ create_gps_position/src/gps_points_editor.cpp
@@ -4,7 +4,7 @@
 #include <interactive_markers/interactive_marker_server.h>
 #include <interactive_markers/menu_handler.h>
 
-#include <sensor_msgs/Joy.h>
+#include <sensor_msgs/NavSatFix.h>
 #include <geometry_msgs/PoseArray.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <visualization_msgs/MarkerArray.h>
@@ -17,18 +17,20 @@
 #include <string>
 #include <fstream>
 #include <math.h>
+
 #ifdef NEW_YAMLCPP
 template<typename T>
-void operator >> (const YAML::Node& node, T& i)
-{
+void operator >> (const YAML::Node& node, T& i){
     i = node.as<T>();
 }
 #endif
+
+
 using namespace visualization_msgs;
 
-class WaypointsEditor{
+class GpsPointEditor{
 public:
-    WaypointsEditor() :
+    GpsPointEditor() :
         filename_(""), fp_flag_(false), rate_(10)
     {
         ros::NodeHandle private_nh("~");
@@ -40,83 +42,15 @@ public:
             if(readFile(filename_)) {
                 fp_flag_ = true;
              } else {
-                ROS_ERROR("Failed loading waypoints file");
+                ROS_ERROR("Failed loading gps data file");
             }
         } else {
-            ROS_ERROR("waypoints file doesn't have name");
+            ROS_ERROR("gps data file doesn't have name");
         }
     }
 
-	void publishMarkerDescription(){
-
-        for (int i=0; i!=waypoints_.size(); i++){
-            Marker marker;
-            marker.type = Marker::TEXT_VIEW_FACING;
-            marker.text = std::to_string(i);
-            marker.header.stamp = ros::Time::now();
-            std::stringstream name;
-            name << "waypoint";
-            marker.ns = name.str();
-            marker.id = i;
-            marker.pose.position = waypoints_.at(i);
-            marker.pose.position.z = 3.0;
-            marker.scale.z = 2.0;
-            marker.color.r = 0.0;
-            marker.color.g = 0.0;
-            marker.color.b = 0.0;
-            marker.color.a = 1.0;
-            marker.action = visualization_msgs::Marker::ADD;
-            marker_description_.markers.push_back(marker);
-        }
-        marker_description_pub_.publish(marker_description_);
-    }
-
-    bool readFile(const std::string &filename){
-        waypoints_.clear();
-        try{
-            std::ifstream ifs(filename.c_str(), std::ifstream::in);
-            if(ifs.good() == false){
-                return false;
-            }
-			YAML::Node node;
-			#ifdef NEW_YAMLCPP
-			node = YAML::Load(ifs);
-			#else
-			YAML::Parser parser(ifs);
-			parser.GetNextDocument(node);
-			#endif
-			
-			#ifdef NEW_YAMLCPP
-			const YAML::Node &gp_node_tmp = node["gps_position"];
-			const YAML::Node *gp_node = gp_node_tmp ? &gp_node_tmp : NULL;
-			#else
-		   	const YAML::Node *gp_node = node.FindValue("gps_positionss");
-		    #endif
-
-            if(gp_node != NULL){
-                for(int i=0; i < gp_node->size(); i++){
-                    geometry_msgs::Point point;
-
-                    (*gp_node)[i]["point"]["x"] >> point.x;
-                    (*gp_node)[i]["point"]["y"] >> point.y;
-                    (*gp_node)[i]["point"]["z"] >> point.z;
-
-                    waypoints_.push_back(point);
-                }
-            }else{
-				ROS_WARN("nya");
-                return false;
-            }
-		}catch(YAML::ParserException &e){
-				ROS_WARN_STREAM( e.what() );
-				return false;
-		}catch(YAML::RepresentationException &e){
-				ROS_WARN_STREAM( e.what() );
-            	return false;
-		}
-        return true;
-    }
-
+	bool readFile(const std::string &filename);
+	void publishMarkerDescription();	
     void run() {
         while(ros::ok()){
             rate_.sleep();
@@ -126,19 +60,96 @@ public:
 
 private:
     ros::Publisher marker_description_pub_;
-    std::vector<geometry_msgs::Point> waypoints_;
     visualization_msgs::MarkerArray marker_description_;
     std::string filename_;
     bool fp_flag_;
     ros::Rate rate_;
+	struct GpsData{
+		geometry_msgs::Point RvizPoint;
+		sensor_msgs::NavSatFix GpsPoint;
+	};
+    std::vector<GpsData> g_waypoints_;
 };
 
+
+bool GpsPointEditor::readFile(const std::string &filename){
+	g_waypoints_.clear();
+	try{
+		std::ifstream ifs(filename.c_str(), std::ifstream::in);
+		if(ifs.good() == false){
+			return false;
+		}
+		YAML::Node node;
+		#ifdef NEW_YAMLCPP
+			node = YAML::Load(ifs);
+		#else
+			YAML::Parser parser(ifs);
+			parser.GetNextDocument(node);
+		#endif
+			
+		#ifdef NEW_YAMLCPP
+			const YAML::Node &gp_node_tmp = node["gps_position"];
+			const YAML::Node *gp_node = gp_node_tmp ? &gp_node_tmp : NULL;
+		#else
+			const YAML::Node *gp_node = node.FindValue("gps_positionss");
+		#endif
+		
+		if(gp_node != NULL){
+			GpsData g_data;
+			for(int i=0; i < gp_node->size(); i++){
+				//geometry_msgs::Point point;
+				(*gp_node)[i]["point"]["x"] >> g_data.RvizPoint.x;
+				(*gp_node)[i]["point"]["y"] >> g_data.RvizPoint.y;
+				(*gp_node)[i]["point"]["z"] >> g_data.RvizPoint.z;
+				(*gp_node)[i]["point"]["lat"] >> g_data.GpsPoint.latitude;
+				(*gp_node)[i]["point"]["lon"] >> g_data.GpsPoint.longitude;
+			   g_waypoints_.push_back(g_data );
+			}
+			/*for(int i=0; i< g_waypoints_.size(); i++){
+				//pushback確認用
+			  	ROS_INFO("%lf",g_waypoints_[i].RvizPoint.x);
+			}*/
+		}else{
+			return false;
+		}
+	}catch(YAML::ParserException &e){
+		ROS_WARN_STREAM( e.what() );
+		return false;
+	}catch(YAML::RepresentationException &e){
+		ROS_WARN_STREAM( e.what() );
+		return false;
+	}
+    return true;
+}
+
+void GpsPointEditor::publishMarkerDescription(){
+	for (int i=0; i!=g_waypoints_.size(); i++){
+    	 Marker marker;
+		 marker.type = Marker::TEXT_VIEW_FACING;
+		// marker.text = std::to_string(i);
+		 marker.header.stamp = ros::Time::now();
+		 std::stringstream name;
+		 name << "gps_point";
+		 marker.ns = name.str();
+		 marker.id = i;
+		// marker.pose.position = g_waypoints_.at(i);
+		 marker.pose.position.z = 3.0;
+		 marker.scale.z = 2.0;
+		 marker.color.r = 0.0;
+		 marker.color.g = 0.0;
+		 marker.color.b = 0.0;
+		 marker.color.a = 1.0;
+		 marker.action = visualization_msgs::Marker::ADD;
+		 marker_description_.markers.push_back(marker);
+	}
+	marker_description_pub_.publish(marker_description_);
+}
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "pose_marker");
   ros::Time::init();
-  WaypointsEditor w_edit;
-  w_edit.run();
+  GpsPointEditor g_edit;
+  g_edit.run();
 
   return 0;
 }
